2accc88c205492b6292586431bdfbae97d15308f
omegamaster86 <masya868686@gmail.com>
2025-05-15 10:17:53 +0900

    Expo Tech Blogへのリンクを追加しました。

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-5xl mx-auto">
 				<LinkCard title="Zennへ移動" href="https://zenn.dev/" />
 				<LinkCard title="qiitaへ移動" href="https://qiita.com/" />
+				<LinkCard title="Expo Tech Blogへ移動" href="https://expo.dev/blog" />
 				<LinkCard
 					title="ubie Tech Blogへ移動"
 					href="https://ubie-inc.notion.site/Ubie-e5ca482892574b198160de93b5ba067a"
