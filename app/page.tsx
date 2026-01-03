d4008d6bd80019e618d0f11fde084452e4e5701e
omegamaster <masya868686@gmail.com>
2026-01-03 16:53:10 +0900

    ホームページの背景に宇宙のビジュアルを追加し、リンクフォームとリンクリストを新しいレイアウトで表示するように変更。

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { LinkForm } from "./components/link-form";
 import { LinkList } from "./components/link-list";
+import { SpaceBackground } from "./components/space-background";
 
 export default function Home() {
 	return (
-		<div className="min-h-screen bg-linear-to-r/srgb from-indigo-500 to-teal-400 p-4">
-			<LinkForm />
-			<LinkList />
+		<div className="relative min-h-screen p-4 bg-[#030014]">
+			<SpaceBackground />
+			<div className="relative z-10">
+				<LinkForm />
+				<LinkList />
+			</div>
 		</div>
 	);
 }
